4b0a5a89ed4aad71bad06fdb96d998291ce4d8f7
Ian Meikle <imeikle@gmail.com>
2014-10-16 20:35:38 +0100

    Updated script so table contains only 'mean' and 'std' variables.

--- run_analysis.R
+++ run_analysis.R
@@ -8,4 +8,15 @@ features <- read.table("./features.txt", stringsAsFactors = FALSE)
 X_cols <- features[,2]
 # Name the columns using the cryptic feature names
 colnames(X_df) <- X_cols
-str(X_df)
+#str(X_df)
+Y_df <- read.table("./test/y_test.txt")
+Y_df <- rbind(read.table("./train/y_train.txt"))
+Y_vec <- Y_df[[1]]
+Y_fac <- factor(Y_vec,labels = fac_act)
+subj_df <- read.table("./test//subject_test.txt")
+subj_df <- rbind(read.table("./train//subject_train.txt"))
+subj_vec <- subj_df[[1]]
+data <- data.frame(subj_vec,Y_fac,X_df)
+data_tbl <- tbl_df(data)
+data_sel <- select(data_tbl, subj_vec, Y_fac, contains("mean"), contains("std"))
+
