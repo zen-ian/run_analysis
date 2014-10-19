a50e276ea9fb1a35a2e710998ce042068edf4af9
Ian Meikle <imeikle@gmail.com>
2014-10-19 20:43:20 +0100

    First draft finished. Requires review.

--- run_analysis.R
+++ run_analysis.R
@@ -1,9 +1,14 @@
+setwd("/Users/ianmeikle/Development/Coursera/GetCleanData/UCI HAR Dataset")
 # First, read in the data from the test subjects
 X_df <- read.table("./test/X_test.txt")
 # Then merge the data from the train subjects
 X_df <- rbind(read.table("./train/X_train.txt"))
 # Load the cryptic feature names
 features <- read.table("./features.txt", stringsAsFactors = FALSE)
+# Load the activity names as factors
+activities <- read.table("./activity_labels.txt")
+# Grab 2nd col as vector
+fac_act <- activities[[2]]
 # Extract the feature names as a character vector
 X_cols <- features[,2]
 # Name the columns using the cryptic feature names
@@ -17,6 +22,8 @@ subj_df <- read.table("./test//subject_test.txt")
 subj_df <- rbind(read.table("./train//subject_train.txt"))
 subj_vec <- subj_df[[1]]
 data <- data.frame(subj_vec,Y_fac,X_df)
+library(dplyr)
 data_tbl <- tbl_df(data)
 data_sel <- select(data_tbl, subj_vec, Y_fac, contains("mean"), contains("std"))
-
+data_grp <- group_by(data_sel, Y_fac, subj_vec)
+data_summ < - summarise_each(data_grp, funs(mean))
